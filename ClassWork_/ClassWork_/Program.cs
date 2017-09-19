b8a232f9be3e03b9d00057627a8b1164be89255d
tempest240 <tempest240@mail.ru>
2017-09-19 19:33:36 +0300

    Init Commit

--- ClassWork_/ClassWork_/Program.cs
+++ ClassWork_/ClassWork_/Program.cs
@@ -10,134 +10,149 @@ namespace ClassWork_
     {
         static void Main(string[] args)
         {
-                        
-        }
-    }
+            Book book = new Book();
+            List<Technical> bookList = new List<Technical>();
 
-    class Book
-    {
-        //  variables (public, protected, private)
-        protected string _author;        
-        protected string _name;        
-        protected int _pages;
-        
-        //  constructors
-        public Book()
-        {
-            _author = "unknown";
-            _name = "unknown";
-            _pages = 0;
-        }
-        public Book(string author, string name)
-        {
-            _author = author;
-            _name = name;
-            _pages = 0;
-        }
-        public Book (string author, string name, int pages)
-        {
-            _author = author;
-            _name = name;
-            _pages = pages;
-        }
+            bookList.Add(new Technical("space", "Hokking", "nonFiction"));
+            bookList.Add(new Technical("botanica", "Hokking", "nonFiction"));
+            bookList.Add(new Technical("apace", "jokking", "nonFiction"));
+            bookList.Add(new Technical("jpace", "jhokking", "nonFiction"));
+            bookList.Add(new Technical("dpace", "fgkking", "nonFiction"));
+            bookList.Add(new Technical("lpace", "tokking", "nonFiction"));
 
-        //  properties
-        public string author
-        {
-            get { return _author; }
-        }
+            bookList.Sort((b1, b2) => b1.scienceArea.CompareTo(b2.scienceArea));
 
-        public string name
-        {
-            get { return _name; }
-        }
-
-        public int pages
-        {
-            get { return _pages; }
-        }
-
-        //  methods
-        public void Print()
-        {
-            Console.Write("{0} {1} {2}", author, name, pages);
-        }
-    }
-
-    class Technical : Book
-    {
-        private string _scienceArea;      
-
-        public Technical() : base()
-        {
-            _scienceArea = "unknown";
-        }
-
-        public Technical(string scienceArea) : base()
-        {
-            _scienceArea = scienceArea;
-        }        
-
-        public Technical(string scienceArea, string author, string name) : base(author, name)
-        {
-            _scienceArea = scienceArea;
-        }
-
-        public Technical(string scienceArea, string author, string name, int pages) : base(author, name, pages)
-        {
-            _scienceArea = scienceArea;
-        }
-
-        public string scienceArea
-        {
-            get { return _scienceArea; }
-        }
-
-        public void Print()
-        {
-            Console.Write("{0} {1} {2} {3}", author, name, pages, scienceArea);
-        }
-    }   
-    class Fiction : Book
-    {
-
-    }
-
-    class Library
-    {
-        private List<Book> book = new List<Book>();
-
-        public void AddBook(Book b)
-        {            
-            book.Add(b);
-        }     
-        
-        public void Print()
-        {
-            foreach(Book i in book)
+            foreach(Technical b in bookList)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(b.scienceArea);
             }
-        }  
+        }
 
-        public void PrintTechnical()
+        class Book
         {
-            foreach(Book i in book)
+            //  variables (public, protected, private)
+            protected string _author;
+            protected string _name;
+            protected int _pages;
+
+            //  constructors
+            public Book()
             {
-                if(i.name.GetType().Equals("Technical"))
+                _author = "unknown";
+                _name = "unknown";
+                _pages = 0;
+            }
+            public Book(string author, string name)
+            {
+                _author = author;
+                _name = name;
+                _pages = 0;
+            }
+            public Book(string author, string name, int pages)
+            {
+                _author = author;
+                _name = name;
+                _pages = pages;
+            }
+
+            //  properties
+            public string author
+            {
+                get { return _author; }
+            }
+
+            public string name
+            {
+                get { return _name; }
+            }
+
+            public int pages
+            {
+                get { return _pages; }
+            }
+
+            //  methods
+            public void Print()
+            {
+                Console.Write("{0} {1} {2}", author, name, pages);
+            }
+        }
+
+        class Technical : Book
+        {
+            private string _scienceArea;
+
+            public Technical() : base()
+            {
+                _scienceArea = "unknown";
+            }
+
+            public Technical(string scienceArea) : base()
+            {
+                _scienceArea = scienceArea;
+            }
+
+            public Technical(string scienceArea, string author, string name) : base(author, name)
+            {
+                _scienceArea = scienceArea;
+            }
+
+            public Technical(string scienceArea, string author, string name, int pages) : base(author, name, pages)
+            {
+                _scienceArea = scienceArea;
+            }
+
+            public string scienceArea
+            {
+                get { return _scienceArea; }
+            }
+
+            public void Print()
+            {
+                Console.Write("{0} {1} {2} {3}", author, name, pages, scienceArea);
+            }
+        }
+        class Fiction : Book
+        {
+
+        }
+
+        class Library
+        {
+            private List<Book> book = new List<Book>();
+
+            public void AddBook(Book b)
+            {
+                book.Add(b);
+            }
+
+            public void Print()
+            {
+                foreach (Book i in book)
                 {
-                    Console.WriteLine(i + " book");
+                    Console.WriteLine(i);
                 }
             }
-        }
 
-        public void PrintFiction()
-        {
-            foreach(Book i in book)
+            public void PrintTechnical()
             {
-                if (i.name.GetType().Equals("Fiction"))
+                foreach (Book i in book)
                 {
-                    Console.WriteLine(i + " book"); 
+                    if (i.name.GetType().Equals("Technical"))
+                    {
+                        Console.WriteLine(i + " book");
+                    }
+                }
+            }
+
+            public void PrintFiction()
+            {
+                foreach (Book i in book)
+                {
+                    if (i.name.GetType().Equals("Fiction"))
+                    {
+                        Console.WriteLine(i + " book");
+                    }
                 }
             }
         }
